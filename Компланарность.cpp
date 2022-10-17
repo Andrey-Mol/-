b7400352845a8a9e4285a55420a8aee188c697dd
Andrey-Mol <115218203+Andrey-Mol@users.noreply.github.com>
2022-10-17 23:24:25 +0500

    Update Компланарность.cpp

--- Компланарность.cpp
+++ Компланарность.cpp
@@ -1,39 +1,60 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
+
 //размер матрицы
 const int n = 3;
-const int m = 3;
 
 //объявление квадратной матрицы 3x3 и заполнение нулями:
-int matrix[n][m] = {};
+int matrix[n][n] = {};
 
 //создание промежуточной матрицы для вычисления всех определителей:
 int temp_matrix[n][n];
 
+//выбор фигуры
+int f() {
+	cout << "Выберите необходимую фигуру: Тетраэдр - t  Параллелепипед - p \n";
+	float fig = 0;
+	char k;
+	while (fig == 0) {
+		cin >> k;
+		if (k == 't') {
+			fig = 6;
+		}
+		else if ((k == 'p')) {
+			fig = 1;
+		}
+		else {
+			cout << "Повторите попытку \n";
+		}
+	}
+	return fig;
+}
+
 //ввод данных в матрицу 3x3
 void entering_coef() {
-	cout << "Ax Ay Az \nBx By Bz \nCx Cy Cz\n";
 	char answer = ' ';
-		while (answer != 'y')
-		{
-			cout << "Введите координаты векторов, для определения их компланарности: \n";
-			for (int i = 0; i < n; ++i) {
-				for (int j = 0; j < m; ++j) {
-					cin >> matrix[i][j];
-				}
+	while (answer != 'y')
+	{
+		cout << "Введите координаты векторов, образующих фигуру, для определения её объёма. Формат данных: \n \n{Ax; Ay; Az} \n{Bx; By; Bz} \n{Cx; Cy; Cz}\n \
+		(Без разделительных знаков и скобок!)\n";
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				cin >> matrix[i][j];
 			}
-			cout << "Правильно ли введены данные?: y/n \n";
-			for (int i = 0; i < n; ++i) {
-				for (int j = 0; j < m; ++j) {
-					cout << matrix[i][j] << " ";
-				}
-				cout << endl;
-			}
-			cin >> answer;
-
 		}
+		cout << "Правильно ли введены данные?: y/n \n";
+		for (int i = 0; i < n; ++i) {
+			cout << "{ ";
+			for (int j = 0; j < n; ++j) {
+				cout << matrix[i][j] << " ";
+			}
+			cout << "}" << endl;
+		}
+		cin >> answer;
+
+	}
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
 			temp_matrix[i][j] = matrix[i][j];
@@ -53,18 +74,17 @@ int determinant(int matr[n][n]) {
 int main() {
 	//для установки русского языка в выводе:
 	setlocale(LC_ALL, "Russian");
-
 	entering_coef();
 	cout << "---------------------------------------------" << endl;
 
-	int det_main = determinant(temp_matrix);
-	cout << "Определитель:" << det_main << endl;
-
+	int det_main = abs(determinant(temp_matrix));
 	if (det_main == 0) {
 		cout << "Они компланарны";
 	}
 	else {
-		cout << "Они не компланарны"<< endl;
+		float v;
+		v = det_main;
+		cout << "Они не компланарны \n" << "Объём = " << v / f() << endl;
 	}
 	return 0;
 }
